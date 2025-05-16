f078ea26af1aa56f743f3d3999f0e7282327b193
Bryce Wooster <40742705+BryceWDesign@users.noreply.github.com>
2025-05-16 09:15:03 -0700

    Add main entry point coordinating touch input, motion control, and logging

--- enhanced_controller/main.py
+++ enhanced_controller/main.py
@@ -1,22 +1,35 @@
 # enhanced_controller/main.py
 
-from control.hand_controller import HandController
-from interface.hand_gui import HandGUI
-from interface.feedback_panel import FeedbackPanel
-from data.motion_logger import MotionLogger
-from intelligence.motion_annotator import MotionAnnotator
+from enhanced_controller.controller import EnhancedController
+from enhanced_controller.sensors.touch_sensor import TouchSensor
+from enhanced_controller.utils.motion_range import MotionRange
+from enhanced_controller.data.motion_logger import MotionLogger
+import time
 
 def main():
+    controller = EnhancedController()
+    touch_sensor = TouchSensor()
     logger = MotionLogger()
-    controller = HandController(logger=logger)
-    gui = HandGUI(controller)
-    feedback = FeedbackPanel(controller)
-    annotator = MotionAnnotator(logger)
 
-    print("[System] Launching enhanced controller...")
-    gui.run()
-    feedback.listen()
-    annotator.monitor()
+    try:
+        while True:
+            touched = touch_sensor.read_touch()
+            if touched:
+                logger.log("Touch detected - initiating grip")
+                grip_angles = {
+                    'thumb': MotionRange.clamp_motion('thumb', 45),
+                    'index': MotionRange.clamp_motion('index', 90),
+                    'middle': MotionRange.clamp_motion('middle', 90),
+                    'ring': MotionRange.clamp_motion('ring', 90),
+                    'pinky': MotionRange.clamp_motion('pinky', 90)
+                }
+                controller.move_all_fingers(grip_angles)
+            else:
+                logger.log("No touch detected - relaxing hand")
+                controller.relax_hand()
+            time.sleep(1)
+    except KeyboardInterrupt:
+        logger.log("Program terminated by user")
 
 if __name__ == "__main__":
     main()
